9248f4ac7cbd57cb224fdc45cf36bc26935f4d47
Jakub Trafimovich <5243212@gmail.com>
2023-05-23 17:22:15 +0200

    Refactored form page

--- src/pages/FormPage/index.js
+++ src/pages/FormPage/index.js
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from 'react';
-import uniqid from "uniqid";
 import { func } from "prop-types";
 import {
   phoneNumberFormatValidator,
@@ -8,14 +7,15 @@ import {
 } from "../../shared/validators";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { useOutsideClick } from '../../shared/hooks';
 
 import "./styles.css"
 
-const FormPage = ({onSubmit, onPrev, onNext}) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+const FormPage = ({ onSubmit, onPrev, onNext }) => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
   const [errors, setErrors] = useState({
     name: null,
     email: null,
@@ -23,122 +23,122 @@ const FormPage = ({onSubmit, onPrev, onNext}) => {
   });
   const [isShown, setIsShown] = useState(false);
 
-  const wrapperRef = useRef(null);
-
-  useOutsideClick(wrapperRef, () => {
-    setIsShown(false)
-  });
-
-
   const validate = () => {
     const error = {
-      name: (!fullNameValidator(name)) && "A full name must be present",
-      phone: (!phoneNumberFormatValidator(phone)) && "A valid phone number is required.",
-      email: !emailValidator(email) && "A valid email address is required",
+      name: (!fullNameValidator(form.name)) && "A full name must be present",
+      phone: (!phoneNumberFormatValidator(form.phone)) && "A valid phone number is required.",
+      email: !emailValidator(form.email) && "A valid email address is required",
     };
     setErrors(error);
     return !error.name && !error.email && !error.phone;
   };
 
   const handleSubmit = () => {
-    onSubmit({ name, email, phone });
+    onSubmit({ ...form });
     onNext();
   };
 
-  const handleNameChange = (e) => setName(e.target.value);
-  const handleEmailChange = (e) => setEmail(e.target.value);
-  const handlePhoneChange = (e) => setPhone(e.target.value);
+  const handleChange = (name, value) => {
+    setForm({
+      ...form,
+      [name]: value
+    });
+  };
 
 
   const inputFields = [
     {
       label: "Full name",
-      value: name,
-      onChange: handleNameChange,
+      value: form.name,
+      onChange: (e) => handleChange('name', e.target.value),
       error: errors.name,
     },
     {
       label: "Email address",
-      value: email,
-      onChange: handleEmailChange,
+      value: form.email,
+      onChange: (e) => handleChange('email', e.target.value),
       error: errors.email,
     },
     {
       label: "Phone number",
-      value: phone,
-      onChange: handlePhoneChange,
+      value: form.phone,
+      onChange: (e) => handleChange('phone', e.target.value),
       error: errors.phone,
     },
   ];
 
   const renderInput = field => (
     <div className="inputContainer" key={field.label}>
-        <div className="form-group">
-            <label>{field.label}</label>
-            <span className="text-danger font-weight-bold">*</span>
-        </div>
-        <input
-          className="form-control"
-          type="text"
-          value={field.value}
-          onChange={field.onChange}
-          placeholder={field.label}
-        />
-         <span className="text-danger inputError">{field.error}</span>
+      <div className="form-group">
+        <label>{field.label}</label>
+        <span className="text-danger font-weight-bold">*</span>
       </div>
+      <input
+        className="form-control"
+        type="text"
+        value={field.value}
+        onChange={field.onChange}
+        placeholder={field.label}
+      />
+      <span className="text-danger inputError">{field.error}</span>
+    </div>
   );
 
   return (
-      <form className="form-container">
-          <h1>Form</h1>
-          {inputFields.map(field => renderInput(field))}
-          <div className="btn-container">
+    <form className="form-container">
+      <h1>Form</h1>
+      {inputFields.map(field => renderInput(field))}
+      <div className="btn-container">
+        <Button
+          type="button"
+          className="btn btn-success my-btn"
+          onClick={() => {
+            if (validate()) {
+              setIsShown(true)
+            }
+          }}
+        >
+          Submit
+        </Button>
+        <div>
+          <Modal
+            show={isShown}
+            backdrop="static"
+          >
+            <Modal.Header>
+              <Modal.Title>Confirm data</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <p>Do you confirm the sending of the data?</p>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button
+                variant="secondary"
+                onClick={() => setIsShown(false)}
+              >
+                Close
+              </Button>
               <Button
                 type="button"
-                className="btn btn-success my-btn"
-                onClick={() => {
-                  if (validate()) {
-                    setIsShown(true)
-                  }
-                }}
+                variant="primary"
+                onClick={() => handleSubmit()}
               >
                 Submit
               </Button>
-              <div ref={wrapperRef}>
-                  <Modal
-                      show={isShown}
-                      backdrop="static"
-                  >
-                      <Modal.Header closeButton>
-                        <Modal.Title>Confirm data</Modal.Title>
-                      </Modal.Header>
-
-                      <Modal.Body>
-                        <p>Do you confirm the sending of the data?</p>
-                      </Modal.Body>
-
-                      <Modal.Footer>
-                        <Button variant="secondary">Close</Button>
-                        <Button
-                          type="button"
-                          variant="primary"
-                          onClick={() => handleSubmit()}
-                        >
-                          Submit
-                        </Button>
-                      </Modal.Footer>
-                  </Modal>
-              </div>
-              <button
-                  type="button"
-                  className="btn btn-warning"
-                  
-                  onClick={() => onPrev()}
-              >
-                  Back
-              </button>
-          </div>
-      </form>
+            </Modal.Footer>
+          </Modal>
+        </div>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={() => onPrev()}
+        >
+          Back
+        </button>
+      </div>
+    </form>
   );
 };
 
